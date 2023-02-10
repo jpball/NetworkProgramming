6c3f84f4754f3276c2eeb273521889485cc8cb1e
Jordan B <balljordan28@gmail.com>
2023-02-10 14:53:18 -0600

    Removed memory leak

--- p1/client.cpp
+++ p1/client.cpp
@@ -22,10 +22,6 @@ using namespace std;
 
 void GetOptions(int argc, char* argv[], int& portNumber, string& serverAddress, bool& debugFlag, uint32_t& msDelay, uint32_t& numDG);
 void DisplayMenu(char* argv[]);
-const ClientDatagram BuildDatagram(char* buffer, uint32_t seqNum, char* message);
-inline char* GetDatagramMessage(char* datagramBuffer);
-
-#define BUFFER_SIZE 1024
 
 #define INVALID_OPTION 9
 #define HELP_MENU_RV 10
@@ -33,6 +29,8 @@ inline char* GetDatagramMessage(char* datagramBuffer);
 #define SERVER_CONNECTION_ERROR_RV 12
 #define MESSAGE_SEND_ERROR_RV 13
 #define MESSAGE_RECV_ERROR_RV 14;
+
+#define MESSAGE "jball3"
 
 int main(int argc, char * argv[])
 {
@@ -50,11 +48,16 @@ int main(int argc, char * argv[])
 	ssize_t bytesReceived;
 	socklen_t incomingSocketLength = sizeof(serverSockAddr);
 
-	char buffer[BUFFER_SIZE];
+	const size_t MESSAGE_SIZE = strlen(MESSAGE) + 1; // +1 for the null terminator
+	const ssize_t EXP_OUTGOING_DATAGRAM_SIZE = sizeof(ClientDatagram) + MESSAGE_SIZE; // Expected  size of the outgoing datagram's metadata and message
+	const ssize_t EXP_INCOMING_DATAGRAM_SIZE = sizeof(ServerDatagram); // Expected size of the incoming datagram from the server
+	char* outgoingBuffer = nullptr;
+	char* incomingBuffer = nullptr;
 	try
 	{
 		GetOptions(argc, argv, serverPort, serverAddress, isDebugMode, microsecDelay, numDatagrams);
 
+		#pragma region ConnectionSetup
 		// Create our outgoing data socket
 		if(isDebugMode) cout << "Opening socket..." << endl;
 		udpSocketNumber = socket(AF_INET, SOCK_DGRAM, 0);
@@ -83,6 +86,8 @@ int main(int argc, char * argv[])
 		memmove(&serverSockAddr.sin_addr.s_addr, serverHostEnt->h_addr, serverHostEnt->h_length);
 		serverSockAddr.sin_port = htons(serverPort);
 
+		#pragma endregion
+
 
 		// We wanna send a certain number of datagrams out
 		for(size_t dgNum = 0; dgNum < numDatagrams; dgNum++)
@@ -91,25 +96,23 @@ int main(int argc, char * argv[])
 				BUILDING THE DATAGRAM
 			*/
 			if(isDebugMode) cout << "Creating datagram..." << endl;
-			char message[] = "HelloWorld";
-			size_t messageSize = strlen(message) + 1;
+			outgoingBuffer = (char*)malloc( EXP_OUTGOING_DATAGRAM_SIZE );
 
-			ClientDatagram* outgoingDG = (ClientDatagram*)buffer;
-			ssize_t fullDatagramSize = sizeof(ClientDatagram) + messageSize;
-
+			ClientDatagram* outgoingDG = (ClientDatagram*)outgoingBuffer;
 
 			// Set payload length
-			outgoingDG->payload_length = messageSize + sizeof(ClientDatagram);
+			outgoingDG->payload_length = MESSAGE_SIZE;
 			outgoingDG->sequence_number = dgNum;
-			strcpy(buffer + sizeof(ClientDatagram), message);
+
+			strcpy(outgoingBuffer + sizeof(ClientDatagram), MESSAGE); // Copy the message after the datagram metadata
 
 			if(isDebugMode)
 			{
 				cout << "Datagram created!";
 				cout << " sn: " 	<< outgoingDG->sequence_number  << " {" 	<< sizeof(outgoingDG->sequence_number)	<< " bytes}";
 				cout << " pl: " 	<< outgoingDG->payload_length	<< " {" 	<< sizeof(outgoingDG->payload_length) 	<< " bytes}";
-				cout << " m:  " 	<< message					 	<< " {" 	<< messageSize							<< " bytes}";
-				cout << " sz: " 	<< fullDatagramSize 			<< " bytes" << endl;
+				cout << " m:  " 	<< MESSAGE					 	<< " {" 	<< MESSAGE_SIZE							<< " bytes}";
+				cout << " sz: " 	<< EXP_OUTGOING_DATAGRAM_SIZE 	<< " bytes" << endl;
 			}
 
 			// Prepare ByteOrdering of Datagram for network
@@ -117,39 +120,62 @@ int main(int argc, char * argv[])
 			outgoingDG->sequence_number = htonl(outgoingDG->sequence_number);
 
 			/*
-				SEND THE DATAGRAM
+				SENDING THE DATAGRAM
 			*/
 			if(microsecDelay > 0) usleep(microsecDelay); // Delay before the next outgoing datagram if we want to
 
 			if(isDebugMode) cout << "Attempting to send payload..." << endl;
-			ssize_t bytesSent = sendto(udpSocketNumber, (void*)buffer, fullDatagramSize, 0, (struct sockaddr*) &serverSockAddr, sizeof(serverSockAddr));
+			ssize_t bytesSent = sendto(udpSocketNumber, outgoingBuffer, EXP_OUTGOING_DATAGRAM_SIZE, 0, (struct sockaddr*) &serverSockAddr, sizeof(serverSockAddr));
 			// Check if there was an error with sending
-			if(bytesSent != fullDatagramSize)
+			if(bytesSent != EXP_OUTGOING_DATAGRAM_SIZE)
 			{
-				cerr << "Sent: " << bytesSent << " expected to send: " << fullDatagramSize << endl;
+				cerr << "Sent: " << bytesSent << " expected to send: " << EXP_OUTGOING_DATAGRAM_SIZE << endl;
 				perror("sendto()");
 				throw MESSAGE_SEND_ERROR_RV;
 			}
 
+			free(outgoingBuffer);
+			outgoingBuffer = nullptr;
+			outgoingDG = nullptr;
+
 			/*
-				CHECK FOR RECIEVING
+			=======================
+				RECIEVING
+			=======================
 			*/
-			bytesReceived = recvfrom(udpSocketNumber, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &serverSockAddr, &incomingSocketLength);
-			if(errno == EAGAIN || errno == EWOULDBLOCK) continue; // These flags are set when there was nothing to read
+
+			incomingBuffer = (char*) malloc( EXP_INCOMING_DATAGRAM_SIZE );
+			bytesReceived = recvfrom(udpSocketNumber, incomingBuffer, EXP_INCOMING_DATAGRAM_SIZE, 0, (struct sockaddr *) &serverSockAddr, &incomingSocketLength);
+
+			if(errno == EAGAIN || errno == EWOULDBLOCK)
+			{
+				// These flags are set when there was nothing to read
+				free(incomingBuffer);
+				incomingBuffer = nullptr;
+				continue; // We just wanna move on to the next datagram
+			}
+
+
 			if(bytesReceived > 0)
 			{
 				// We recieved something
-				ServerDatagram incomingDG = *((ServerDatagram*)buffer);
-				incomingDG.datagram_length = ntohs(incomingDG.datagram_length);
-				incomingDG.sequence_number = ntohl(incomingDG.sequence_number);
+				ServerDatagram* incomingDG = (ServerDatagram*)(incomingBuffer);
+				incomingDG->datagram_length = ntohs(incomingDG->datagram_length); // network to host short
+				incomingDG->sequence_number = ntohl(incomingDG->sequence_number); // network to host long
+
+				// Print out info alerting us to a datagram reception
 				cout << "Recieved datagram from server: ";
-				cout << "     seq: " << incomingDG.sequence_number << " len: " << incomingDG.datagram_length << endl;
+				cout << "     seq: " << incomingDG->sequence_number << " len: " << incomingDG->datagram_length << endl;
 			}
 			else
 			{
+				// We have a real error at this point
 				perror("recvfrom()");
 				throw MESSAGE_RECV_ERROR_RV;
 			}
+
+			free(incomingBuffer);
+			incomingBuffer = nullptr;
 		}
 
 	}
@@ -163,34 +189,20 @@ int main(int argc, char * argv[])
 		close(udpSocketNumber);
 	}
 
-	return retval;
-}
-
-const ClientDatagram BuildDatagram(char* buffer, uint32_t seqNum, char* message)
-{
-	ClientDatagram cdg;
-	memset(&cdg, 0, sizeof(ClientDatagram)); // Clear the struct values
-
-	cdg.sequence_number = seqNum;
-	cdg.payload_length = strlen(message);
-	memcpy(buffer, &cdg, sizeof(ClientDatagram)); // Copy our datagram
-	memcpy(buffer + sizeof(ClientDatagram), message, cdg.payload_length); // Copy our message
-
-	if(strcmp(GetDatagramMessage(buffer), message) != 0)
+	if(incomingBuffer != nullptr)
 	{
-		cerr << "**** ERROR: Message improperly transfered to datagram |";
-		cerr << "Message: {" << message << "} but transferred: {" << GetDatagramMessage(buffer) << "}" << endl;
-		throw 99;
+		free(incomingBuffer);
+		incomingBuffer = nullptr;
 	}
 
-	return cdg;
-}
+	if(outgoingBuffer != nullptr)
+	{
+		free(outgoingBuffer);
+		outgoingBuffer = nullptr;
+	}
 
-inline char* GetDatagramMessage(char* datagramBuffer)
-{
-	return (datagramBuffer + sizeof(ClientDatagram));
+	return retval;
 }
-
 
 void GetOptions(int argc, char* argv[], int& portNumber, string& serverAddress, bool& debugFlag, uint32_t& msDelay, uint32_t& numDG)
 {
@@ -207,46 +219,51 @@ void GetOptions(int argc, char* argv[], int& portNumber, string& serverAddress, 
 			}
 			case('y'):
 			{
-				// -y introduces a delay between sending datagrams
+				// -y (number) introduces a delay between sending datagrams
 				msDelay = atoi(optarg);
 				break;
 			}
 			case('n'):
 			{
+				// -n (number) overrides the default number of datagrams to send
 				numDG = atoi(optarg);
 				break;
 			}
 			case ('s'):
 			{
-				// -s allows you to override the default of 127.0.0.1.
+				// -s (serverIP) allows you to override the default of 127.0.0.1.
 				serverAddress = string(optarg);
 				break;
 			}
 			case ('p'):
 			{
-				// -p allows you to override the default destination port of 39390.
+				// -p (port) allows you to override the default destination port of 39390.
 				portNumber = atoi(optarg);
 				break;
 			}
 			case ('d'):
 			{
+				// -d enables debugging
 				debugFlag = true;
+				numDG = 5; // For debugging...set only 5 datagrams (will be overrided if -n is supplied)
 				break;
 			}
 			default:
 			{
-				cerr << "ERROR, Invalid option [" << c << "] with argument: [" << optarg << "]" << endl;
+				cerr << "ERROR, Invalid option [" << c << "]" << endl;
 				throw INVALID_OPTION;
 			}
 		}
 	}
 }
 //--
-void DisplayMenu(char* argv[])
+void DisplayMenu( char* argv[])
 {
-	cerr << argv[0] << "options:" << endl;
+	cerr << argv[0] << " options:" << endl;
 	cerr << "	-h displays help" << endl;
 	cerr << "	-s server_address (required)" << endl;
-	cerr << "	-p port_number (required)" << endl;
-	cerr << "   -m message (required)" << endl;
+	cerr << "	-p port_number    (required)" << endl;
+	cerr << "   -m message        (required)" << endl;
+	cerr << "   -n datagram_count" << endl;
+	cerr << "   -y delay_in_ms" << endl;
 }
