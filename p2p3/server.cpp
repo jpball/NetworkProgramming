392c1b062bc1ce4140f1021d5955c90e82e3d1c7
Jordan B <balljordan28@gmail.com>
2023-03-23 13:09:07 -0500

    More updates for the server

--- p2p3/server.cpp
+++ p2p3/server.cpp
@@ -23,7 +23,7 @@ using namespace std;
 #define DEFAULT_PORT 9838
 #define DEFAULT_ADDRESS "127.0.0.1"
 #define LISTEN_BACKLOG 5
-#define DATA_BUFFER_SIZE 2048
+#define DATA_BUFFER_SIZE 80
 
 
 void GetOpt(int argc, char* argv[], int& portNum);
@@ -34,6 +34,7 @@ void BindListeningSocket(const int& socketFD, const sockaddr_in& serverSockAddr)
 void SetSocketToListenForConnections(const int& socketFD, const int backlogSize);
 int GetNewConnection(const int& listenSocketFD, sockaddr_in& clientSockAddr);
 void SetSocketOptions(const int& socketFD);
+bool AddMessageToStrBuilder(string messageToAdd, stringstream& strBuilder);
 
 int main(int argc, char* argv[])
 {
@@ -64,40 +65,27 @@ int main(int argc, char* argv[])
 
         char dataBuffer[DATA_BUFFER_SIZE];
         
-        while(true) // Run for each client
+        while(true) // Outer client loop (for each client)
         {
             memset((void*)&clientSockAddr, 0, sizeof(clientSockAddr)); // Reset our client addr struct
 
             connectedSocket_fd = GetNewConnection(incomingConnectionSocket_fd, clientSockAddr);
             cout << "Recieved connection from " << inet_ntoa(clientSockAddr.sin_addr) << " from port " << clientSockAddr.sin_port << endl;
 
-            stringstream strBuilder;
+            stringstream fullMessageStrBuilder;
             bool readyToSendBack = false;
 
-            while(true) // Loop until client disconnected
+            while(true) // Inner Loop until client disconnected
             {
                 memset(&dataBuffer, 0, DATA_BUFFER_SIZE); // Reset data buffer
                 
-
                 ssize_t br = recv(connectedSocket_fd, (void*)dataBuffer, DATA_BUFFER_SIZE, 0);
                 if(br > 0)
                 {
                     // No issues
-                    string recvStr = string(dataBuffer);
-                    size_t overWordIndex;
-                    if((overWordIndex = recvStr.find("OVER")) != string::npos)
-                    {
-                        // Found the word OVER
-                        string subStr = recvStr.substr(0, overWordIndex);
-                        if(subStr.size() > 0) strBuilder << subStr;
-                        readyToSendBack = true;
-                    }
-                    else
-                    {
-                        // Recv string does not contain OVER
-                        strBuilder << recvStr;
-                    }
-                    cout << "Server Recieved: " << strBuilder.str() << endl;
+                    readyToSendBack = AddMessageToStrBuilder(string(dataBuffer), fullMessageStrBuilder);
+                    // Print out what the server has stored so far
+                    cout << "Server Recieved: " << fullMessageStrBuilder.str() << endl;
                 }
                 else
                 {
@@ -106,6 +94,8 @@ int main(int argc, char* argv[])
                     {
                         // Client disconnected
                         cout << "Client closed its connection" << endl;
+                        close(connectedSocket_fd);
+                        connectedSocket_fd = -1;
                         break;
                     }
                     else
@@ -116,19 +106,27 @@ int main(int argc, char* argv[])
 
                 if(readyToSendBack)
                 {
-                    size_t sizeOfBuiltMessage = strBuilder.str().size();
+                    /*
+                        Get the details of our outgoing message
+
+                        Ex.
+                        Line Length: 15 OVER
+                    */ 
+                    size_t sizeOfBuiltMessage = fullMessageStrBuilder.str().size();
                     string fullMessageToSend = "Line Length: " + to_string(sizeOfBuiltMessage) + " OVER";
-                    ssize_t expectedBytesToSend = fullMessageToSend.size() + 1;
+                    ssize_t expectedBytesToSend = fullMessageToSend.size() + 1; // Number of bytes we are to send, including null terminator
 
                     cout << "Server sending: " << fullMessageToSend << endl;
 
+                    // Send our line length message to the client
                     ssize_t sentBytes = send(connectedSocket_fd, fullMessageToSend.c_str(), fullMessageToSend.size() + 1, 0);
                     if(sentBytes != expectedBytesToSend)
                     {
+                        // Bytes sent doesn't match expected count
                         perror("ERROR, send()");
                     }
-                    
-                    strBuilder.str(""); // Clear the string builder
+
+                    fullMessageStrBuilder.str(""); // Clear the string builder
                     readyToSendBack = false; // Reset our sending flag
                 }
             }
@@ -144,16 +142,49 @@ int main(int argc, char* argv[])
         close(incomingConnectionSocket_fd);
     }
 
+    // Close our connected socket if it is open
     if(connectedSocket_fd > 0)
     {
         close(connectedSocket_fd);
     }
 
 
-    cout << "Returning..." << retVal << endl;
+    cerr << "Returning with code: " << retVal << endl;
     return retVal;
 }
 //--
+/*
+    Parse the given message, and add it to our accumualting string builder
+    THe code word 'OVER' will indicate the message is finished being recv, and needs to be sent
+
+    Returns a bool indicating if the designated ending word was given, indicating if we are ready to send back
+*/
+bool AddMessageToStrBuilder(string messageToAdd, stringstream& strBuilder)
+{
+    // No issues
+    bool doesMessageContainOVER = false;
+
+    size_t overWordIndex;
+    if((overWordIndex = messageToAdd.find("OVER")) != string::npos)
+    {
+        // Found the word OVER
+        string subStr = messageToAdd.substr(0, overWordIndex);
+        if(subStr.size() > 0) strBuilder << subStr; // There is substance to send
+        doesMessageContainOVER = true;
+    }
+    else
+    {
+        // Recv string does not contain OVER
+        strBuilder << messageToAdd;
+    }
+
+    return doesMessageContainOVER;
+}
+//--
+/* 
+Enable certain settings for the given socket
+ - SO_REUSEADDR -> Reuse ports for other sockets
+*/
 void SetSocketOptions(const int& socketFD)
 {
     int optval(1); // true (1)
@@ -163,6 +194,10 @@ void SetSocketOptions(const int& socketFD)
     }
 }
 //--
+/*
+    Will accept an incoming connection request via the listening socket
+    And will return the corresponding socket
+*/
 int GetNewConnection(const int& listenSocketFD, sockaddr_in& clientSockAddr)
 {
     socklen_t clientAddrSize = socklen_t(sizeof(clientSockAddr));
@@ -175,6 +210,9 @@ int GetNewConnection(const int& listenSocketFD, sockaddr_in& clientSockAddr)
     return newClientSocket;
 }
 //--
+/*
+    Create our initial socket to listen for incoming connections
+*/
 int CreateListeningSocket()
 {
     int retSocket = socket(PF_INET, SOCK_STREAM, 0);
@@ -186,6 +224,9 @@ int CreateListeningSocket()
     return retSocket;
 }
 //--
+/*
+    Bind our listening socket to use our server connection info
+*/
 void BindListeningSocket(const int& socketFD, const sockaddr_in& serverSockAddr)
 {
     if(bind(socketFD, ((const sockaddr*)&serverSockAddr), socklen_t(sizeof(serverSockAddr))) < 0)
@@ -195,6 +236,9 @@ void BindListeningSocket(const int& socketFD, const sockaddr_in& serverSockAddr)
     }
 }
 //--
+/*
+    Set the given socket to listen for backlogSize number of incoming connections
+*/
 void SetSocketToListenForConnections(const int& socketFD, const int backlogSize)
 {
     if(listen(socketFD, LISTEN_BACKLOG) == -1)
@@ -204,6 +248,9 @@ void SetSocketToListenForConnections(const int& socketFD, const int backlogSize)
     }
 }
 //--
+/*
+    Parse the command line options, potentially changing our port number
+*/
 void GetOpt(int argc, char* argv[],  int& portNum)
 {
     int c;
@@ -226,6 +273,9 @@ void GetOpt(int argc, char* argv[],  int& portNum)
     }
 }
 //--
+/*
+    Print our usage menu to the standard error
+*/
 void PrintMenu()
 {
     cerr << "Usage: " << endl;
